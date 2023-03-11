7984e66a2249bfed32209b1200693d8b45e2bd06
mikolajmki <mikol4m@gmail.com>
2023-03-11 20:31:36 +0100

    position fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,28 +11,29 @@ export const Header = () => {
 
     const menuHandler = (menuOpened) => {
         if (document.documentElement.clientWidth <= 768) {
-            return menuOpened ? { opacity: "100", display: "block" } : {};
+            return menuOpened ? { display: "block" } : {}
         }
     }
 
     return (
         <div className={css.container}>
 
-            <div className={css.name}>
+            <Link to={"/"} className={css.name}>
                 <span>Baw<span>aria.pl</span> </span>
-            </div>
+            </Link>
+
             <div className={css.menuButton} onClick={() => setMenuOpened((prev) => !prev)}>
                 <UilBars/>
             </div>
             <div className={css.menu} style={ menuHandler(menuOpened) }>
-                <ul className={css.list}>
-                { categories.map((category, i) => {
-                    return (
-                        <li><a href={"#" + category.name}>{category.name}</a></li>
-                    )
-                }) }
-                </ul>
-            </div>
+                    <ul className={css.list}>
+                    { categories.map((category, i) => {
+                        return (
+                            <li><a href={"#" + category.name}>{category.name}</a></li>
+                        )
+                    }) }
+                    </ul>
+                </div>
         </div>
     )
 }
