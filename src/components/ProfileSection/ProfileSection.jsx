fabe0425df884ef3b03ac25b022f55a92510e007
mikolajmki <mikol4m@gmail.com>
2023-04-04 23:21:11 +0200

    profile

---
+++ src/components/ProfileSection/ProfileSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import css from './ProfileSection.module.css';
+
+export const ProfileSection = () => {
+    return (
+        <div className={css.container}>
+            <div className={css.profile}>
+            Profile
+            </div>
+        </div>
+    )
+}
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import css from './Profile.module.css';
 import { Header } from "../../components/Header/Header";
+import { ProfileSection } from "../../components/ProfileSection/ProfileSection";
 
 export const Profile = () => {
     return (
         <>
         <Header/>
-        <div className={css.container}>Profile</div>
+        <div className={css.container}>
+            <ProfileSection/>
+        </div>
         </>
     )
 }
